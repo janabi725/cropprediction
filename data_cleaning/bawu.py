274e812212cf815ff78753282313c8736d9189ce
janabi725 <sohail.akram@web.de>
2026-04-15 09:51:16 +0200

    Further data cleaning. File named 'winterweizen_geernet.csv' contains the yield for every Landkreis in Baden-Württemberg and excluded the Landkreise with no data

--- data_cleaning/bawu.py
+++ data_cleaning/bawu.py
@@ -1,19 +1,35 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/41241-01-03-4.csv', sep = ';')
+bawu_raw = pd.read_csv('/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/41241-01-03-4.csv', sep = ';')
 
-df.insert(0, 'Jahr', 0)
+bawu_raw.insert(0, 'Jahr', 0)
 
-df['Jahr'] = np.where(df['Winterweizen'].isna(), df['Kreis-Id'], np.nan)
+bawu_raw['Jahr'] = np.where(bawu_raw['Winterweizen'].isna(), bawu_raw['Kreis-Id'], np.nan)
 
-df['Jahr'] = df['Jahr'].ffill()
+bawu_raw['Jahr'] = bawu_raw['Jahr'].ffill()
 
-df = df.dropna()
+bawu_order = bawu_raw.dropna()
+
+file_all = "/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/bawu_order.csv"
+
+bawu_order.to_csv(file_all)
+
+bawu_winterweizen = bawu_order.drop(columns=["Roggen und Wintermenggetreide", "Wintergerste", "Sommergerste", "Hafer", "Triticale", "Kartoffeln", "Zuckerrüben", "Winterraps", "Silomais"])
 
 
-print(df.head())
+file_winterweizen = "/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/winterweizen.csv"
 
-filepath = "/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/clean.csv"
+bawu_winterweizen.to_csv(file_winterweizen)
 
-df.to_csv(filepath)
+file_winterweizen_yield = "/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/winterweizen_geerntet.csv"
+
+bawu_winterweizen_slash = bawu_winterweizen[~bawu_winterweizen['Winterweizen'].str.contains('/')]
+
+bawu_winterweizen_nodash = bawu_winterweizen_slash[~bawu_winterweizen_slash['Winterweizen'].str.contains('.', regex = False)]
+
+bawu_winterweizen_nodash.to_csv(file_winterweizen_yield)
+
+print(bawu_winterweizen_nodash.head())
+
+
