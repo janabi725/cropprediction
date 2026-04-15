0e96609fb1bbf6f08747d1f92e1c3e190893aa38
janabi725 <sohail.akram@web.de>
2026-04-15 09:54:04 +0200

    Structured the data folder for better overview and organisation

--- data_cleaning/bawu.py
+++ data_cleaning/bawu.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-bawu_raw = pd.read_csv('/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/41241-01-03-4.csv', sep = ';')
+bawu_raw = pd.read_csv('/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/raw/41241-01-03-4.csv', sep = ';')
 
 bawu_raw.insert(0, 'Jahr', 0)
 
@@ -11,18 +11,18 @@ bawu_raw['Jahr'] = bawu_raw['Jahr'].ffill()
 
 bawu_order = bawu_raw.dropna()
 
-file_all = "/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/bawu_order.csv"
+file_all = "/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/intermediate/bawu_order.csv"
 
 bawu_order.to_csv(file_all)
 
 bawu_winterweizen = bawu_order.drop(columns=["Roggen und Wintermenggetreide", "Wintergerste", "Sommergerste", "Hafer", "Triticale", "Kartoffeln", "Zuckerrüben", "Winterraps", "Silomais"])
 
 
-file_winterweizen = "/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/winterweizen.csv"
+file_winterweizen = "/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/intermediate/winterweizen.csv"
 
 bawu_winterweizen.to_csv(file_winterweizen)
 
-file_winterweizen_yield = "/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/winterweizen_geerntet.csv"
+file_winterweizen_yield = "/Users/sohailludin/Desktop/01 Arbeit/01 Universität /03 Master/02 2. Semester/06 Softwarearchitekturen/Labor/cropprediction/data/clean/winterweizen_geerntet.csv"
 
 bawu_winterweizen_slash = bawu_winterweizen[~bawu_winterweizen['Winterweizen'].str.contains('/')]
 
