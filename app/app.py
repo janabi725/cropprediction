698f4e1492c40b68997f9c22df70f9d73e893beb
alexandergrischancew <alexander.grischancew@yahoo.de>
2026-04-24 09:59:41 +0200

    optimized code

--- app/app.py
+++ app/app.py
@@ -5,26 +5,39 @@ from streamlit_folium import st_folium
 import pandas as pd
 import joblib
 
+
+@st.cache_resource
+def load_rf_model():
+    return joblib.load('../ml-pipeline/rf_ertragsmodell.pkl')
+
+@st.cache_data
+def get_predictions():
+    gdf = gpd.read_file("landkreise_bawu_sauber.geojson")
+    
+    input_data = pd.read_csv('../ml-pipeline/features_2024_für_app.csv')
+
+    for col in gdf.select_dtypes(include=['datetime64', 'datetimetz']).columns:
+        gdf[col] = gdf[col].astype(str)
+            
+    for col in input_data.select_dtypes(include=['datetime64', 'datetimetz']).columns:
+        input_data[col] = input_data[col].astype(str)
+    
+    model = load_rf_model()
+    feature_cols = ['NDVI', 'Temp', 'Niederschlag']
+    input_data['Prognose_dt_ha'] = model.predict(input_data[feature_cols])
+    
+    input_data['Kreis-Id'] = input_data['Kreis-Id'].astype(str).str.zfill(5)
+    gdf['ARS'] = gdf['ARS'].astype(str).str.zfill(5)
+    
+    gdf_final = gdf.merge(input_data[['Kreis-Id', 'Prognose_dt_ha']], left_on='ARS', right_on='Kreis-Id', how='left')
+    
+    gdf_final['geometry'] = gdf_final['geometry'].simplify(tolerance=0.001, preserve_topology=True)
+    
+    return gdf_final
+
 st.title("Ertragsprognose Baden-Württemberg")
 
-model = joblib.load('../ml-pipeline/rf_ertragsmodell.pkl')
-input_data = pd.read_csv('../ml-pipeline/features_2024_für_app.csv')
-features_for_model = input_data[['NDVI', 'Temp', 'Niederschlag']]
-input_data['Prognose_dt_ha'] = model.predict(features_for_model)
-df_predictions = input_data[['Kreis-Id', 'Prognose_dt_ha']].copy()
-
-# read geo jason
-gdf = gpd.read_file("landkreise_bawu_sauber.geojson")
-
-# clean data
-for col in df_predictions.select_dtypes(include=['datetime64', 'datetimetz']).columns:
-    df_predictions[col] = df_predictions[col].astype(str)
-
-for col in gdf.select_dtypes(include=['datetime64', 'datetimetz']).columns:
-    gdf[col] = gdf[col].astype(str)
-
-df_predictions['Kreis-Id'] = df_predictions['Kreis-Id'].astype(str).str.zfill(5)
-gdf['ARS'] = gdf['ARS'].astype(str).str.zfill(5)
+gdf = get_predictions()
 
 # map creation    
 m = folium.Map(location=[48.5, 9.0], zoom_start=8, tiles=None)
@@ -40,7 +53,7 @@ folium.TileLayer(
 folium.Choropleth(
     geo_data=gdf,
     name='Ertragsprognose',
-    data=df_predictions,
+    data=gdf,
     columns=['Kreis-Id', 'Prognose_dt_ha'],
     key_on='feature.properties.ARS',
     fill_color='YlGn',              
@@ -49,4 +62,10 @@ folium.Choropleth(
     legend_name='Prognostizierter Ertrag (dt/ha)'
 ).add_to(m)
 
-st_folium(m, width=700, height=500)
+st_data = st_folium(
+    m, 
+    width=800, 
+    height=600,
+    returned_objects=["last_active_drawing"],
+    zoom=8
+)
